b37ef579cace6799d42512b5a7d21ca780664c08
Kartal <salimkartal@outlook.com.tr>
2020-10-14 13:17:21 +0300

    ilk gönderme

---
+++ MvcStok/Controllers/GuvenlikController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class GuvenlikController : Controller
+    {
+        // GET: Guvenlik
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult GirisYap(TBLKULLANICI t)
+        {
+            var bilgiler = db.TBLKULLANICI.FirstOrDefault(x => x.AD == t.AD && x.SIFRE == t.SIFRE);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.AD, false);
+                return RedirectToAction("Index", "Musteri");
+            }
+            else
+            {
+                return View();
+            }
+        }
+       
+
+
+    }
+}
---
+++ MvcStok/Controllers/KayitController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class KayitController : Controller
+    {
+        // GET: Kayit
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult KayıtOl()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KayıtOl(TBLKULLANICI p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("KayıtOl");
+            }
+            db.TBLKULLANICI.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+    }
+
+    
+}
